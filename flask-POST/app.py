89ada601172d0ab8c750b43f29469b8845dc955e
dreamjh20 <dreamjh20@gmail.com>
2021-07-25 19:51:03 +0900

    [CREATE] http POST with Html Css Rendering On Flask

---
+++ flask-POST/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+	return render_template('index.html')
+
+@app.route("/post",methods=['POST'])
+def post():
+	value = request.form['input']
+	msg = "%s 어서와~" %value
+	return render_template('welcome.html', value = msg)
+
+if __name__ == "__main__":
+		app.run()
