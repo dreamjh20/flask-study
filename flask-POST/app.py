e7a4ca0d6611f10729f50f388a18620be5fc7841
dreamjh20 <dreamjh20@gmail.com>
2021-07-25 20:53:51 +0900

    [UPDATE] Link Database on Inner Local Server on Flask

--- flask-POST/app.py
+++ flask-POST/app.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template, request
+import sqlite3
+import datetime as dt
+
 app = Flask(__name__)
+
+
 
 @app.route("/")
 def hello():
@@ -8,8 +13,16 @@ def hello():
 @app.route("/post",methods=['POST'])
 def post():
 	value = request.form['input']
-	msg = "%s 어서와~" %value
+	msg = "%s 어서 와~" %value
+	print(value)
+	now = dt.datetime.now()
+	nowDatetime = now.strftime('%Y-%m-%d %H:%M:%S')
+	conn = sqlite3.connect('namedb.db', isolation_level=None)
+	cur = conn.cursor()
+	cur.execute("CREATE TABLE IF NOT EXISTS users(username TEXT, regdate TEXT)")
+	cur.execute("INSERT INTO users VALUES(?, ?)", (value, nowDatetime,))
+
 	return render_template('welcome.html', value = msg)
 
 if __name__ == "__main__":
-		app.run(host='10.156.146.106')
+		app.run()
