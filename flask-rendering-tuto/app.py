686c42da885aa18263e202592e85b985e515eff3
dreamjh20 <68009161+dreamjh20@users.noreply.github.com>
2021-07-29 22:06:46 +0900

    [UPDATE] Succeed message

--- flask-rendering-tuto/app.py
+++ flask-rendering-tuto/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def render():
-    print("Succed Rendering")
+    print("Succeed Rendering")
     return render_template("index.html")
 
 if __name__ == "__main__":
