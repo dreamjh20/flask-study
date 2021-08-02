bef8c04cf6d0975f3e2f68eec022892541ea4510
dreamjh20 <dreamjh20@gmail.com>
2021-08-02 18:47:33 +0900

    [UPDATE] Regular Expression NoNumber & POST Method Done

--- flask-name-nickname/app.py
+++ flask-name-nickname/app.py
@@ -1,11 +1,22 @@
+from os import name
 from flask import Flask, render_template, request
 import sqlite3
 
 app = Flask(__name__)
 
+conn = sqlite3.connect("test.db", isolation_level=None)
+cur = conn.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS users (id integer PRIMARY KEY, name text, nickname text)")
+
 @app.route("/")
 def Input():
 	return render_template('index.html')
+
+@app.route("/succeed", methods=['POST'])
+def Post():
+    name = request.form['name']
+    nickname = request.form['nickname']
+    print(name, nickname)
 
 if __name__ == "__main__":
     app.run()
