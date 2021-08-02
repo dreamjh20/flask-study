414a219beabec30b7ede012f54278d20c855b0c3
dreamjh20 <dreamjh20@gmail.com>
2021-08-02 13:27:26 +0900

    [UPDATE]Front-end

--- flask-name-nickname/app.py
+++ flask-name-nickname/app.py
@@ -1,2 +1,11 @@
 from flask import Flask, render_template, request
 import sqlite3
+
+app = Flask(__name__)
+
+@app.route("/")
+def Input():
+	return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run()
