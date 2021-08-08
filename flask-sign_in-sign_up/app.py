e1c71747b269540e78f2696c4938670da4f136fb
dreamjh20 <dreamjh20@gmail.com>
2021-08-08 22:56:38 +0900

    [UPDATE] Move Page

--- flask-sign_in-sign_up/app.py
+++ flask-sign_in-sign_up/app.py
@@ -3,9 +3,18 @@ import sqlite3
 
 app = Flask(__name__)
 
-@app.route("/")
-def main_page():
-    return("HELLO")
+@app.route('/')
+def main_Page():
+    print("MAIN")
+    return render_template('home.html')
+
+@app.route('/signin')
+def sign_in_Page():
+    return render_template('sign_in_page.html')
+
+@app.route('/signup')
+def sign_up_Page():
+    return render_template('sign_up_page.html')
 
 if __name__ == "__main__":
     app.run()
