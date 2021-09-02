26d10648e1d97f3df2fd1b6ccdcd7b61b220a14a
dreamjh20 <dreamjh20@gmail.com>
2021-09-02 20:23:12 +0900

    FEAT: GET Git Repository

--- flask-github-api/app.py
+++ flask-github-api/app.py
@@ -22,11 +22,24 @@ def get_info():
     url2 = f'https://api.github.com/repos/dreamjh20/embedded/commits'
     
     response = requests.get(url).json()
-   
     print(response)
     follower = response['followers']
+    print(follower)
     following = response['following']
+    print(following)
     return 'INFO'
+
+
+@app.route('/firstcommit', methods=['GET'])
+def first_commit():
+    if request.args.get("username") is not None:
+        user_name=  request.args.get("username")
+    if request.args.get("repository") is not None:
+        user_repository=  request.args.get("repository")
+    print(user_name)
+    print(user_repository)
+    return "FC"
+
 
 if __name__ == "__main__":
     app.run()
