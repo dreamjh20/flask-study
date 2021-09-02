974d477bc868032c33acae286352e75b17fef6ca
dreamjh20 <dreamjh20@gmail.com>
2021-09-02 20:12:34 +0900

    FEAT: Follower Following using api.github

--- flask-github-api/app.py
+++ flask-github-api/app.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template, request
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import requests
+import json
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+
     print('HOME')
     return render_template('home.html')
 
@@ -10,9 +17,16 @@ def home():
 def get_info():
     if request.args.get("username") is not None:
         user_name=  request.args.get("username")
-    if request.args.get("username") is not None:
-        my_repository=  request.args.get("repository")
-    return (my_repository)
+    print(user_name)
+    url=f'https://api.github.com/users/{user_name}'
+    url2 = f'https://api.github.com/repos/dreamjh20/embedded/commits'
+    
+    response = requests.get(url).json()
+   
+    print(response)
+    follower = response['followers']
+    following = response['following']
+    return 'INFO'
 
 if __name__ == "__main__":
     app.run()
