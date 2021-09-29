fec91562149fcd502864c2b7d3c99190d7c61df9
dreamjh20 <dreamjh20@gmail.com>
2021-09-29 23:10:10 +0900

    ADD: INIT

--- flask-email-html/app/__init__.py
+++ flask-email-html/app/__init__.py
@@ -1,1 +1,19 @@
 # app init
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+
+import config
+
+def html_email_blueprint(app : Flask):
+    from .view.email import email_blueprint
+    app.register_blueprint(email_blueprint)
+
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+
+    html_email_blueprint(app)
+
+    return app
