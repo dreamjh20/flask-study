d788d197ad56cc413e42b0205dd5453b3b063323
dreamjh20 <dreamjh20@gmail.com>
2021-09-30 15:31:28 +0900

    FEAT: Send Json Js

--- flask-email-html/static/js/main.js
+++ flask-email-html/static/js/main.js
@@ -1,6 +1,18 @@
-window.onload = function(){
-    var hw = document.getElementById('btn');
-    hw.addEventListener('click', function(){
-        alert('Submitted!');
-    })
-}
+function registerFood(evt) {
+    // evt.preventDefault(); /* POST 이벤트 중지 */
+    // const foodname = evt.target.foodname.value;
+    // const foodsize = evt.target.foodsize.value;
+    // if (!foodname || !foodsize) {
+    //   return alert('이름과 개수를 모두 입력하세요');
+    // }
+  
+    const email = evt.target.email.value;
+    const url = 'http://127.0.0.1:5000';
+    const data = { email };
+    fetch('/email', {
+      method: 'POST',
+      body: JSON.stringify(data),
+    });
+  }
+  
+  document.querySelector('form').addEventListener('submit', registerFood);
