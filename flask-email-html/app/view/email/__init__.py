3d00f52f63714258b149677076ae059f6a0bdfd1
dreamjh20 <dreamjh20@gmail.com>
2021-09-30 15:30:52 +0900

    UPDATE: Return

--- flask-email-html/app/view/email/__init__.py
+++ flask-email-html/app/view/email/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify
 from flask_restful import Api
 
-email_blueprint = Blueprint('send', __name__, url_prefix='/send')
+email_blueprint = Blueprint('home', __name__, url_prefix='/')
 email_api = Api(email_blueprint)
 
 from .mail import SendEmail
--- flask-email-html/app/view/email/mail.py
+++ flask-email-html/app/view/email/mail.py
@@ -1,6 +1,8 @@
 from flask_restful import Resource
+from flask import Flask, jsonify, request
 
 class SendEmail(Resource):
     def post(self):
         print("EMAIL")
-        return 'email'
+        
+        return jsonify({"message":"succeed"})
