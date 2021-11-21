faf1c68bf969d0d32376d77d1248c817d9e5b108
dreamjh20 <dreamjh20@gmail.com>
2021-11-21 23:04:29 +0900

    FEAT: File Display

--- flask-file-upload/app.py
+++ flask-file-upload/app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
+import os
+
 app = Flask(__name__)
+
+imgFolder = os.path.join('static', 'image')
+
+app.config['UPLOAD_FOLDER'] = imgFolder
 
 @app.route('/')
 def home():
@@ -9,9 +15,17 @@ def home():
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        f = request.files['myfile']
-        f.save(secure_filename(f.filename))
-        return 'SUCCEED'
+        f = request.files['myfile']  #파일 받아오기
+        f.save(secure_filename(f.filename)) #암호화
+        #print(f.filename) #파일 이름 출력
+        return_str = f.filename + " SUCCEED"
+        return return_str
+
+@app.route('/image') #url 지정
+def imgshow():
+    image=os.path.join(app.config['UPLOAD_FOLDER'], 'jang.jpg')
+    return render_template("imgshow.html", show_image = image)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
