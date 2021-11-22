03aa606bccc6f591f56b202fd78142305eeac037
dreamjh20 <dreamjh20@gmail.com>
2021-11-22 21:52:48 +0900

    ADD: JWT Instance add environment variable

--- flask-jwt-tuto/app.py
+++ flask-jwt-tuto/app.py
@@ -3,6 +3,13 @@ from flask_jwt_extended import *
 
 app = Flask(__name__) #Flask에서 인스턴스 생성
 
+app.config.update(  #Key를 환경 변수에 등록
+    DEBUG = True,
+    JWT_SECRET_KEY = "Moontong"
+)
+
+jwt = JWTManager(app) #jwt을 flask 앱에 등록
+
 @app.route("/")
 def jwt_tuto():
     return "<h1>MoonTong!</h1>"
