2408995889d7a8997f32cd13259f7d707522691e
dreamjh20 <dreamjh20@gmail.com>
2021-11-22 21:58:06 +0900

    FEAT: Login and Create Access Token from json

--- flask-jwt-tuto/app.py
+++ flask-jwt-tuto/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_jwt_extended import *
 
 app = Flask(__name__) #Flask에서 인스턴스 생성
@@ -18,6 +18,23 @@ jwt = JWTManager(app) #jwt을 flask 앱에 등록
 def jwt_tuto():
     return "<h1>MoonTong!</h1>"
 
-    
+@app.route("/login", methods=['POST'])
+def login_func():
+    get_data = request.get_json()
+    user_id = get_data['id']
+    user_pw = get_data['pw']
+    #json form 에서 id와 password 파싱
+
+    if (admin_id == user_id and admin_pw == user_pw):
+        return jsonify(
+            result = "SUCCESS",
+            access_token = create_access_token(identity=user_id, expires_delta=False)  #create_access_tocken 으로 토큰 반환    identity는 고유 식별자, expires_delta는 토큰 만료일자 False는 무기한
+        )
+
+    else: 
+        return jsonify(
+			result = "Failure"
+	    )
+
 if __name__ == '__main__':
 	app.run(port = 5000, debug = True)
