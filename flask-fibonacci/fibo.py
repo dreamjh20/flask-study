b6596bb7ac7c212d30d043e60676b02f784ebc55
dreamjh20 <dreamjh20@gmail.com>
2021-12-07 10:54:56 +0900

    SET: Value

---
+++ flask-fibonacci/fibo.py
@@ -0,0 +1,19 @@
+class Fibo():
+    
+    def func_fibo(n):
+        a = 1
+        b = 1
+        result = []
+        for i in range(n):
+            result.append(a)
+            a = b
+            b = a + b
+        return result
+
+    def generator_fibo(n):
+        a = 1
+        b = 1
+        for i in range(n):
+            yield a
+            a = b
+            b = a + b
