e33c0c7b8c97a9f6cf04cbfbd0095ee151cf5b7e
dreamjh20 <dreamjh20@gmail.com>
2021-12-07 10:54:41 +0900

    FEAT: Return Returning Time

--- flask-fibonacci/app.py
+++ flask-fibonacci/app.py
@@ -1,3 +1,4 @@
+from re import DEBUG
 from flask import Flask, request, jsonify, render_template
 import time as t
 from fibo import Fibo
@@ -10,9 +11,13 @@ def home():
 
 @app.route("/fibo", methods=['GET'])
 def get_fibo():
+
+    fibo_num = request.args.get('num')
+    fibo_num = int(fibo_num)
+    print(fibo_num)
     started_time = t.time()
 
-    for i in Fibo.func_fibo(500000):
+    for i in Fibo.func_fibo(fibo_num):
         pass
 
     finished_time = t.time()
@@ -20,14 +25,18 @@ def get_fibo():
     print(f"Return Time = {return_time}")
 
     return_time = str(return_time)
-    return return_time
+    returning = "Return Time: " + return_time
+    return returning
 
 @app.route("/fibo", methods=['POST'])
 def post_fibo():
     
+    fibo_num = request.form['num']
+    fibo_num = int(fibo_num)
+    print(fibo_num)
     started_time = t.time()
 
-    for i in Fibo.generator_fibo(500000):
+    for i in Fibo.generator_fibo(fibo_num):
         pass
 
     finished_time = t.time()
@@ -35,7 +44,8 @@ def post_fibo():
     print(f"Return Time = {return_time}")
 
     return_time = str(return_time)
-    return return_time
+    returning = "Return Time: " + return_time
+    return returning
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
