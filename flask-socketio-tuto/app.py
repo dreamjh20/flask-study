eccfb8345ecf1951fcc4b2333a45c42074be96e2
dreamjh20 <dreamjh20@gmail.com>
2021-12-08 10:20:26 +0900

    FEAT: Send Message

--- flask-socketio-tuto/app.py
+++ flask-socketio-tuto/app.py
@@ -1,5 +1,13 @@
-from flask import Flask, app
+from flask import Flask
 from flask_socketio import SocketIO, send
 
+app = Flask(__name__)
+socketIO = SocketIO(app)
+
+@socketIO.on('message')
+def message(msg):
+    print("MESSAGE: " + msg)
+    send(msg, broadcast=True)
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    socketIO.app.run(debug=True, host='0.0.0.0', port=5000)
